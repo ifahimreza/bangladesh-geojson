9c4b027f6df223d4cd882fc2bb4eaa48290eae49
Towsif-Ahamed-Labib <towsif.kuet.ac.bd@gmail.com>
2021-11-16 18:54:07 +0600

    Java class with getter setter

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book {
+
+    private String title;
+    private String author;
+
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+     //getters and setters here 
+}
